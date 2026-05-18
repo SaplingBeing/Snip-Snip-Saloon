9e8a5490ee5ff07383ed96abce2a6b1da532839c
SaplingBeing <justindegrumal28@gmail.com>
2026-05-18 17:38:51 +1200

    Booking added

---
+++ JavaScript/booking.js
@@ -0,0 +1,172 @@
+// Justin D. Grumal 240005134
+$(document).ready(function(){
+    var serviceList = [];
+    var total = 0;
+
+    $("#conta").click(function(){
+        if(total > 0){
+            $("#navTabStaff").removeClass("disabled").click();
+        }
+    });
+    $("#navTabServices").click(function(){
+        $("#navTabStaff").addClass("disabled");
+    });
+    // Services
+    // If no services is selected, a msg will display
+    function isServiceEmpty(){
+        if(serviceList.length === 0){
+            $("#bookingPlaceHolder").removeClass("collapse").addClass("collapse.show");
+        }
+        else{
+            $("#bookingPlaceHolder").removeClass("collapse.show").addClass("collapse");
+        }
+    }
+    // Standard Haircut
+    $("#standardCutBtn").click(function(){
+        // Checks if service is in the list
+        if(serviceList.includes("standardHaircut") == true){ 
+            const target = "standardHaircut";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#standardCutList").remove();
+            total -= 50;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("standardHaircut");
+            $("#bookingList").append("<li id='standardCutList'>Standard Haircut</li>");
+            total += 50;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+    // Scisssor Cut
+    $("#scissorCutBtn").click(function(){
+        if(serviceList.includes("scissorCut") == true){ 
+            const target = "scissorCut";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#scissorCutList").remove();
+            total -= 80;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("scissorCut");
+            $("#bookingList").append("<li id='scissorCutList'>Scissor Cut</li>");
+            total += 80;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+    // Fade
+    $("#fadeCutBtn").click(function(){
+        if(serviceList.includes("fadeCut") == true){ 
+            const target = "fadeCut";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#fadeCutList").remove();
+            total -= 70;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("fadeCut");
+            $("#bookingList").append("<li id='fadeCutList'>Fade</li>");
+            total += 70;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+    // Buzz Cut
+    $("#buzzCutBtn").click(function(){
+        if(serviceList.includes("buzzCut") == true){ 
+            const target = "buzzCut";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#buzzCutList").remove();
+            total -= 40;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("buzzCut");
+            $("#bookingList").append("<li id='buzzCutList'>Buzz Cut</li>");
+            total += 40;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+    // Beard Trim
+    $("#trimBeardBtn").click(function(){
+        if(serviceList.includes("trimBeard") == true){ 
+            const target = "trimBeard";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#trimBeardList").remove();
+            total -= 30;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("trimBeard");
+            $("#bookingList").append("<li id='trimBeardList'>Beard Trim</li>");
+            total += 30;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+    // Full Shave
+    $("#fullBeardBtn").click(function(){
+        if(serviceList.includes("fullBeard") == true){ 
+            const target = "fullBeard";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#fullBeardList").remove();
+            total -= 50;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("fullBeard");
+            $("#bookingList").append("<li id='fullBeardList'>Full Shave</li>");
+            total += 50;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+    // Keratin Treatment
+    $("#treatmentBtn").click(function(){
+        if(serviceList.includes("treatment") == true){ 
+            const target = "treatment";
+            const index = serviceList.indexOf(target);
+            serviceList.splice(index, 1);
+            $("#treatmentList").remove();
+            total -= 500;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        else{
+            serviceList.push("treatment");
+            $("#bookingList").append("<li id='treatmentList'>Keratin Treatment</li>");
+            total += 500;
+            $("#totalPrice").text(total);
+            isServiceEmpty();
+        }
+        console.log(serviceList);
+        console.log(total);
+    });
+});
